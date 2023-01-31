3d241d880d7d0b21f52aa5b1ea10983938f13a15
luoxiao11 <luoxiao.rf@gmail.com>
2023-01-30 15:53:17 -1000

    update1

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -6,13 +6,19 @@ const Landing = () => (
   <Container id="landing-page" fluid className="py-3">
     <Row className="align-middle text-center">
       <Col xs={4}>
-        <Image roundedCircle src="/images/meteor-logo.png" width="150px" />
+          <PeopleFill/>
+          <h1>Mutiple Users</h1>
+          <h5>This address book enables any number of users to register and save their business contacts. You can only see the contacts you have created.</h5>
       </Col>
 
-      <Col xs={8} className="d-flex flex-column justify-content-center">
+      <Col xs={4}>
         <h1>Welcome to this template</h1>
         <p>Now get to work and modify this app!</p>
       </Col>
+
+        <Col xs={4}>
+            <Image roundedCircle src="/images/meteor-logo.png" width="150px" />
+        </Col>
 
     </Row>
   </Container>
