92eaad288ef50feef80d89ba971f9d5bdaf82b4b
luoxiao11 <luoxiao.rf@gmail.com>
2023-01-30 16:56:45 -1000

    update1

---
+++ app/imports/ui/pages/ListContacts.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Meteor } from 'meteor/meteor';
+import { Col, Container, Row } from 'react-bootstrap';
+import { useTracker } from 'meteor/react-meteor-data';
+import { Stuffs } from '../../api/stuff/Stuff';
+import LoadingSpinner from '../components/LoadingSpinner';
+
+/* Renders a table containing all of the Stuff documents. Use <StuffItem> to render each row. */
+const ListContacts = () => {
+  // useTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker
+  const { ready, stuffs } = useTracker(() => {
+    // Note that this subscription will get cleaned up
+    // when your component is unmounted or deps change.
+    // Get access to Stuff documents.
+    const subscription = Meteor.subscribe(Stuffs.userPublicationName);
+    // Determine if the subscription is ready
+    const rdy = subscription.ready();
+    // Get the Stuff documents
+    const stuffItems = Stuffs.collection.find({}).fetch();
+    return {
+      stuffs: stuffItems,
+      ready: rdy,
+    };
+  }, []);
+  return (ready ? (
+    <Container className="py-3">
+      <Row className="justify-content-center">
+        <Col>
+          <Col className="text-center">
+            <h2>List Contacts</h2>
+          </Col>
+        </Col>
+      </Row>
+    </Container>
+  ) : <LoadingSpinner />);
+};
+
+export default ListContacts;
