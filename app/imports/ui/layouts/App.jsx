3e758a26fb121cc80b4cb388236ac3e12d386ecb
luoxiao11 <luoxiao.rf@gmail.com>
2023-01-30 22:54:15 -1000

    practice 2

--- app/imports/ui/layouts/App.jsx
+++ app/imports/ui/layouts/App.jsx
@@ -5,7 +5,7 @@ import { Roles } from 'meteor/alanning:roles';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Landing from '../pages/Landing';
-import ListContact from '../pages/ListContact';
+import ListContact from '../pages/ListContacts';
 import ListStuffAdmin from '../pages/ListStuffAdmin';
 import AddStuff from '../pages/AddStuff';
 import EditStuff from '../pages/EditStuff';
