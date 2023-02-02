71e88a91bfaa69096e821d979c5a34182d7f3dfc
luoxiao11 <luoxiao.rf@gmail.com>
2023-02-01 18:51:42 -1000

    update

--- app/imports/ui/components/ContactAdmin.jsx
+++ app/imports/ui/components/ContactAdmin.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Card, Image } from 'react-bootstrap';
-import {Link} from "react-router-dom";
+import { Link } from 'react-router-dom';
 
-/** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
+/** Renders a single row in the List Stuff tablse. See pages/ListStuff.jsx. */
 const Contact = ({ contact }) => (
   <Card className="h-100">
     <Card.Header>
